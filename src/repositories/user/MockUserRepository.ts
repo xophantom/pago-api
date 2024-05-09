3d09bdcf3ee0c8fad0aa576b0efee7a77a537557
Leo Sperandio <leomartins64@yahoo.com.br>
2024-05-08 21:15:11 -0300

    refactor: refac mockUserRepo

--- src/repositories/user/MockUserRepository.ts
+++ src/repositories/user/MockUserRepository.ts
@@ -2,28 +2,35 @@ import { User } from '@prisma/client';
 import { IUserRepository } from './IUserRepository';
 
 export class MockUserRepository implements IUserRepository {
-  private users: User[] = [];
+  public users: User[] = [];
 
-  async fetchUser(id: string): Promise<User | undefined> {
-    return this.users.find((user) => user.id === id);
-  }
+  fetchUser = jest.fn((id: string): Promise<User | undefined> => {
+    return Promise.resolve(this.users.find((user) => user.id === id));
+  });
 
-  async addUser(user: User): Promise<User> {
+  addUser = jest.fn((user: User): Promise<User> => {
     this.users.push({ ...user });
-    return user;
-  }
+    return Promise.resolve(user);
+  });
 
-  async modifyUser(id: string, modifiedUser: User): Promise<User | undefined> {
-    const index = this.users.findIndex((user) => user.id === id);
-    if (index === -1) return undefined;
-    this.users[index] = { ...modifiedUser };
-    return this.users[index];
-  }
+  modifyUser = jest.fn(
+    (id: string, modifiedUser: User): Promise<User | undefined> => {
+      const index = this.users.findIndex((user) => user.id === id);
+      if (index === -1) return Promise.resolve(undefined);
+      this.users[index] = { ...modifiedUser };
+      return Promise.resolve(this.users[index]);
+    },
+  );
 
-  async removeUser(id: string): Promise<void> {
+  removeUser = jest.fn((id: string): Promise<void> => {
     const index = this.users.findIndex((user) => user.id === id);
     if (index !== -1) {
       this.users.splice(index, 1);
     }
-  }
+    return Promise.resolve();
+  });
+
+  clearUsers = (): void => {
+    this.users = [];
+  };
 }
